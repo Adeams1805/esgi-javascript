fbdc958e3c78eb4a3db8cbf06f31cf44be6c21ec
Maeva <maeva@uxia-agency.com>
2020-10-21 18:26:18 +0200

    type check v1

---
+++ exercice-2/type-check.js
@@ -0,0 +1,9 @@
+function type_check_v1(val, type) {
+    let valType = ";"
+    if(val === null) valType = "null";
+    else if(typeof val === "object" && val.length !== undefined) valType = "array";
+    else valType = typeof val;
+    return valType === type;
+  }
+  
+  console.log(type_check_v1([], "array"));
